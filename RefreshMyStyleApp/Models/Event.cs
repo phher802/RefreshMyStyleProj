6b6487d4b40265e85ea28dfa4e3786a2606e7153
phher802 <your_email@example.com>
2021-01-29 10:42:12 -0600

    Create Video model and event model

---
+++ RefreshMyStyleApp/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RefreshMyStyleApp.Models
+{
+    public class Event
+    {
+        [Key]
+        public int? EventId { get; set; }
+
+        public DateTime? DatePosted { get; set; }
+
+        public DateTime? EventDate { get; set; }
+
+        public string EventTitle { get; set; }
+
+        public string Message { get; set; }
+
+
+    }
+}
---
+++ RefreshMyStyleApp/Models/Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace RefreshMyStyleApp.Models
+{
+    public class Video
+    {
+        [Key]
+        public int? VideoId { get; set; }
+
+        public string VideoTitle { get; set; }
+
+        public IFormFile VideoData { get; set; }
+    }
+}
