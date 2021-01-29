9c15e95abb8fc527396b3b17e8b284d97f961158
phher802 <pher024@gmail.com>
2021-01-28 23:40:46 -0600

    Create user role identity

---
+++ RefreshMyStyleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RefreshMyStyleApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
