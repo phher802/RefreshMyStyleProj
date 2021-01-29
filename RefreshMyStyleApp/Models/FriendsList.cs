0b93e8817c42c99d387b3fe6877c11cc4bbfad7e
phher802 <your_email@example.com>
2021-01-29 11:09:31 -0600

    Add more properties to Image model and create ClaimedList model properties

--- RefreshMyStyleApp/Models/FriendsList.cs
+++ RefreshMyStyleApp/Models/FriendsList.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,11 @@ namespace RefreshMyStyleApp.Models
 {
     public class FriendsList
     {
+        [Key]
+        public int FriendsListId { get; set; }
+
+        [ForeignKey("ClothingEnthusiast")]
+        public int UserId { get; set; }
+        public ClothingEnthusiast User { get; set; }
     }
 }
