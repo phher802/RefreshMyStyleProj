88aa89b6900f503f7647443830b403e564a33256
phher802 <pher024@gmail.com>
2021-02-01 12:03:02 -0600

    Add ActionFilters folder and GlobalRouting class to route users to correct home page based on what role

---
+++ RefreshMyStyleApp/ActionFilters/GlobalRouting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RefreshMyStyleApp.ActionFilters
+{
+    public class GlobalRouting : IActionFilter
+    {
+        private readonly ClaimsPrincipal _claimsPrincipal;
+        public GlobalRouting(ClaimsPrincipal claimsPrincipal)
+        {
+            _claimsPrincipal = claimsPrincipal;
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.RouteData.Values["controller"];
+            if (controller.Equals("Home"))
+            {
+                if (_claimsPrincipal.IsInRole("ClothingEnthusiast"))
+                {
+                    context.Result = new RedirectToActionResult("Index", "ClothingEnthusiast", null);
+                }
+             
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
